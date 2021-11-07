e681305cd37f701a081af93460bc2bedb4208ae7
Freddy Polania <fpolania@gmail.com>
2021-11-07 18:14:04 -0400

    Another package removal

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,6 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    `gatsby-plugin-preact`,
     {
       resolve: `gatsby-plugin-offline`,
       options: {

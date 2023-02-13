6d7bfb26cceb919051f9ba7548f33232dac94767
Fredy Polania <fpolania@gmail.com>
2023-02-12 23:00:18 -0400

    add: GA measurement ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,23 @@ module.exports = {
         additionalData: `@import "${__dirname}/src/assets/scss/global";`,
       },
     },
-    "gatsby-plugin-google-gtag",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-ZQP8T9WP7N", // Google Analytics / GA          
+        ],
+        /* gtagConfig: {
+          optimize_id: "GTM-5LFLQGT",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        }, */
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
     {

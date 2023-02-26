140012c91207d10fd0d62e66eeaf94ff063714cd
Fredy Polania <fpolania@gmail.com>
2023-02-26 15:38:55 -0400

    fix: contacto styles

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -14,45 +14,52 @@ const formulario = {
 const ContactoPage = () => {
   return (
     <Page>
-      <Seo title="Información de contacto de Freddy Polania | @Flaex_" />
-      <h1 className="hidden">Contacto</h1>
-      <StaticImage
-        src="https://res.cloudinary.com/flaex/image/upload/v1677436355/strapi-v4/flaex_contactame_8e534549b5.webp"
-        alt="Expresión @flaex_"
-        className="contacto__imagen"
-      />
-      <p>{formulario.descripcion}</p>
       <div className="contacto">
-        <div className="formulario">
-          <h2>{formulario.titulo}</h2>
+        <div className="contacto__col">
+          <h1 className="hidden">Contacto</h1>
+          <div className="contacto__imagen-wrapper">
+            <StaticImage
+              src="https://res.cloudinary.com/flaex/image/upload/v1677436355/strapi-v4/flaex_contactame_8e534549b5.webp"
+              alt="Expresión @flaex_"
+              className="contacto__imagen"
+            />
+          </div>
+          <p>{formulario.descripcion}</p>
+          <div className="formulario">
+            <h2>{formulario.titulo}</h2>
 
-          <form
-            className="contacto__form"
-            method="POST"
-            data-netlify="true"
-            name="contact"
-            action="/gracias/"
-          >
-            <label>
-              <input type="hidden" name="bot-field" />
-              <input type="hidden" name="form-name" value="contact" />
-            </label>
-            <div>
-              <input type="text" name="name" placeholder="Nombre y Apellido" />
-            </div>
-            <div>
-              <input type="email" name="email" placeholder="Email" />
-            </div>
-            <div>
-              <input type="number" name="phone" placeholder="Teléfono" />
-            </div>
-            <div>
-              <textarea name="message" placeholder="Mensaje" />
-            </div>
-            <input type="submit" value="Enviar ➤" />
-          </form>
+            <form
+              className="contacto__form"
+              method="POST"
+              data-netlify="true"
+              name="contact"
+              action="/gracias/"
+            >
+              <label>
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact" />
+              </label>
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Nombre y Apellido"
+                />
+              </div>
+              <div>
+                <input type="email" name="email" placeholder="Email" />
+              </div>
+              <div>
+                <input type="number" name="phone" placeholder="Teléfono" />
+              </div>
+              <div>
+                <textarea name="message" placeholder="Mensaje" />
+              </div>
+              <input type="submit" value="Enviar ➤" />
+            </form>
 
-          <p className="contacto__mensaje">{formulario.redes}</p>
+            <p className="contacto__mensaje">{formulario.redes}</p>
+          </div>
         </div>
       </div>
     </Page>

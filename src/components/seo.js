25f8a11030e07ae91dc55f6b733c5a76a646c081
Fredy polania <fpolania@gmail.com>
2023-03-05 19:44:43 -0400

    fix: og:image

--- src/components/seo.js
+++ src/components/seo.js
@@ -15,7 +15,7 @@ export const SEO = ({ title, description, pathname, children }) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: `${siteUrl}${image}`,
+    image: image,
     url: `${siteUrl}${pathname || ``}`,
     twitterUsername,
     type,

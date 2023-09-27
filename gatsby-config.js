315e9eb7de564ddeb07975dd72a3a6294757381e
Fredy Polania <fpolania@gmail.com>
2023-09-27 11:01:49 -0400

    fix: site metadata image

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       "Emprendedor, comunicador visual, desarrollador web. Guías, artículos y tutoriales de diseño gráfico y tecnologías web.",
     author: "Fredy Polania",
     image:
-      "https://res.cloudinary.com/flaex/image/upload/v1677437625/strapi-v4/flaex_web_incio_039d1d75c0.webp",
+      "https://res.cloudinary.com/flaex/image/upload/v1695826859/strapi-v4/flaex_seo_index_5a5072083a.webp",
     author: "Fredy Polania",
     type: "website",
     siteUrl: "https://flaex.design",
